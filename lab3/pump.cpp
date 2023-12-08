541f9b77a5ee6a89a54b42011a3acc5e209883e3
time_skipper <sasha.shvech2003@mail.ru>
2023-12-08 21:14:27 +0300

    pump structure and ways from vertex

--- lab3/pump.cpp
+++ lab3/pump.cpp
@@ -2,14 +2,15 @@
 
 class pump{
 private:
-    std::map<int, std::vector<std::string>> ways_to_cycle;
+    std::vector<std::string> ways_to_cycle;
     std::vector<std::string> vertex_cycles;
-    std::map<int, std::vector<std::string>> ways_from_cycle;
+    std::vector<std::string> ways_from_cycle;
 public:
-    void init_pump(automaton auto1, int n);
+    pump(int n, automaton auto1);
 };
 
-void pump::init_pump(automaton auto1, int n) {
+pump::pump(int n, automaton auto1) {
+    this->ways_to_cycle = auto1.get_all_ways_to_all_vertexes()[n];
     this->vertex_cycles = auto1.get_cycles()[n];
-
+    this->ways_from_cycle = auto1.get_all_ways_from_all_vertexes()[n];
 }
