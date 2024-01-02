25c4d7557966fa48ff68b0e16878a2788576577b
time_skipper <sasha.shvech2003@mail.ru>
2024-01-03 02:24:10 +0300

    add pumps

--- lab3/pump.cpp
+++ lab3/pump.cpp
@@ -6,6 +6,7 @@ private:
     std::vector<std::string> vertex_cycles;
     std::vector<std::string> ways_from_cycle;
     int vertex_num;
+    std::vector<std::string> pumps;
 public:
     pump(int n, automaton auto1);
     std::vector<std::string> get_ways_to_cycle();
@@ -18,6 +19,13 @@ pump::pump(int n, automaton auto1) {
     vertex_cycles = auto1.get_cycles()[n];
     ways_from_cycle = auto1.get_all_ways_from_all_vertexes()[n];
     vertex_num = n;
+    for (const auto &to : ways_to_cycle){
+        for (const auto &cycle : vertex_cycles){
+            for (const auto &from : ways_from_cycle){
+                pumps.push_back(to + cycle + from);
+            }
+        }
+    }
 }
 
 std::vector<std::string> pump::get_ways_to_cycle() {
